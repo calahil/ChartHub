15e7c78ca7365cba3140cfbde513b3e905e1944f
Christopher Cowan <calahil@calahilstudios.com>
2024-08-09 10:38:49 -0700

    git

--- RhythmVerseClient/Services/IGoogleDriveClient.cs
+++ RhythmVerseClient/Services/IGoogleDriveClient.cs
@@ -4,17 +4,22 @@ using Google.Apis.Services;
 using Google.Apis.Util.Store;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Configuration.UserSecrets;
+using System.Collections.ObjectModel;
 
 namespace RhythmVerseClient.Services
 {
     public interface IGoogleDriveClient
     {
         Task<string> CreateDirectoryAsync(string directoryName);
+        Task<string> GetDirectoryIdAsync(string directoryName);
         Task<string> UploadFileAsync(string directoryId, string filePath);
         Task DownloadFileAsync(string fileId, string saveToPath);
         Task DeleteFileAsync(string fileId);
         Task<IList<Google.Apis.Drive.v3.Data.File>> ListFilesAsync(string directoryId);
+        Task MonitorDirectoryAsync(string directoryId, TimeSpan pollingInterval, Action<Google.Apis.Drive.v3.Data.File, string> onFileChanged);
         public string RhythmVerseFolderId { get; }
+
+        Task<ObservableCollection<FileData>> GetFileDataCollectionAsync(string directoryId);
     }
 
     public class GoogleDriveClient : IGoogleDriveClient
@@ -36,6 +41,9 @@ namespace RhythmVerseClient.Services
         {
             _driveService = await GetServiceAsync();
             RhythmVerseFolderId = await CreateDirectoryAsync("RhythmVerse");
+            //await MonitorDirectoryAsync(RhythmVerseFolderId, TimeSpan.FromSeconds(30), OnFileChanged);
+
+            var fileDataCollection = await GetFileDataCollectionAsync(RhythmVerseFolderId);
         }
 
         public async Task<DriveService> GetServiceAsync()
@@ -61,6 +69,55 @@ namespace RhythmVerseClient.Services
             });
         }
 
+        private void OnFileChanged(Google.Apis.Drive.v3.Data.File file, string changeType)
+        {
+            if (changeType == "created")
+            {
+                // Handle file creation
+                Console.WriteLine($"File created: {file.Name}");
+            }
+            else if (changeType == "deleted")
+            {
+                // Handle file deletion
+                Console.WriteLine($"File deleted: {file.Name}");
+            }
+        }
+
+        public async Task MonitorDirectoryAsync(string directoryId, TimeSpan pollingInterval, Action<Google.Apis.Drive.v3.Data.File, string> onFileChanged)
+        {
+            var existingFiles = new Dictionary<string, Google.Apis.Drive.v3.Data.File>();
+
+            while (true)
+            {
+                var files = await ListFilesAsync(directoryId);
+
+                foreach (var file in files)
+                {
+                    if (!existingFiles.ContainsKey(file.Id))
+                    {
+                        // New file detected
+                        existingFiles[file.Id] = file;
+                        onFileChanged?.Invoke(file, "created");
+                    }
+                }
+
+                // Check for deletions
+                foreach (var existingFileId in existingFiles.Keys.ToList())
+                {
+                    if (!files.Any(f => f.Id == existingFileId))
+                    {
+                        // File deleted
+                        var deletedFile = existingFiles[existingFileId];
+                        existingFiles.Remove(existingFileId);
+                        onFileChanged?.Invoke(deletedFile, "deleted");
+                    }
+                }
+
+                // Wait for the next poll
+                await Task.Delay(pollingInterval);
+            }
+        }
+
         public async Task<string> CreateDirectoryAsync(string directoryName)
         {
             // Check if the directory already exists
@@ -82,7 +139,7 @@ namespace RhythmVerseClient.Services
             return file.Id;
         }
 
-        private async Task<string> GetDirectoryIdAsync(string directoryName)
+        public async Task<string> GetDirectoryIdAsync(string directoryName)
         {
             var request = _driveService.Files.List();
             request.Q = $"mimeType='application/vnd.google-apps.folder' and name='{directoryName}' and trashed=false";
@@ -140,6 +197,71 @@ namespace RhythmVerseClient.Services
             request.Fields = "files(id, name)";
             var result = await request.ExecuteAsync();
             return result.Files;
+        }
+
+        public async Task<ObservableCollection<FileData>> GetFileDataCollectionAsync(string directoryId)
+        {
+            var files = await ListFilesAsync(directoryId);
+            var fileDataCollection = new ObservableCollection<FileData>();
+
+            foreach (var file in files)
+            {
+                var fileData = await ConvertToFileDataAsync(file);
+                fileDataCollection.Add(fileData);
+            }
+
+            return fileDataCollection;
+        }
+
+        private async Task<FileData> ConvertToFileDataAsync(Google.Apis.Drive.v3.Data.File file)
+        {
+            var fileType = DetermineFileType(file.Name);
+            var imageFile = GetIconForFileType(fileType);
+
+            // Assuming you have a method to get file size from Google Drive
+            long sizeBytes = await GetFileSizeAsync(file.Id);
+
+            return new FileData(
+                displayName: file.Name,
+                filePath: file.Id,
+                watcherFileType: fileType,
+                imageFile: imageFile,
+                sizeBytes: sizeBytes);
+        }
+
+        private WatcherFileType DetermineFileType(string fileName)
+        {
+            var extension = Path.GetExtension(fileName).ToLowerInvariant();
+
+            return extension switch
+            {
+                ".zip" => WatcherFileType.Zip,
+                ".rar" => WatcherFileType.Rar,
+                ".7z" => WatcherFileType.SevenZip,
+                "" => WatcherFileType.Con,
+                _ => WatcherFileType.Unknown,
+            };
+        }
+
+        private string GetIconForFileType(WatcherFileType fileType)
+        {
+            return fileType switch
+            {
+                WatcherFileType.Rar => "rar.png",
+                WatcherFileType.Zip => "zip.png",
+                WatcherFileType.Con => "rb.png",
+                WatcherFileType.SevenZip => "sevenzip.png",
+                WatcherFileType.CloneHero => "clonehero.png",
+                _ => "blank.png",
+            };
+        }
+
+        private async Task<long> GetFileSizeAsync(string fileId)
+        {
+            var request = _driveService.Files.Get(fileId);
+            request.Fields = "size";
+            var file = await request.ExecuteAsync();
+            return file.Size ?? 0;
         }
 
         private string GetMimeType(string fileName)
