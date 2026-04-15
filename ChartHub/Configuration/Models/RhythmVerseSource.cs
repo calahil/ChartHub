695265c22df41550d9c70ca99795bb94806a6a63
Chris Cowan <cowan25@gmail.com>
2026-04-15 02:01:08 -0700

    feat: Update backup API service URLs in configuration for consistency

--- ChartHub/Configuration/Models/RhythmVerseSource.cs
+++ ChartHub/Configuration/Models/RhythmVerseSource.cs
@@ -12,7 +12,7 @@ public static class RhythmVerseSourceUrls
     {
         return source switch
         {
-            RhythmVerseSource.ChartHubMirror => new Uri("http://protail"),
+            RhythmVerseSource.ChartHubMirror => new Uri("http://backupapi.protail"),
             _ => new Uri("https://rhythmverse.co"),
         };
     }
